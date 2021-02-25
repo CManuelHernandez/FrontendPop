a00630e4830e01b87c34c60414aabfcbe25a342d
Manuel Hernandez <cmanuelhm87@gmail.com>
2021-02-25 23:17:45 +0100

    Small Fixes and a button to log off deleting the token of the Local Storage

--- js/controllers/NewSpotOrLoginController.js
+++ js/controllers/NewSpotOrLoginController.js
@@ -13,10 +13,19 @@ export default class NewSpotOrLoginController extends BaseController {
         const usesIsLogged = await dataService.isUserLogged();
         if (usesIsLogged) {
             const newSpotButton = this.element.querySelector('.new-spot-button');
-            newSpotButton.classList.remove('is-hidden');
+            newSpotButton.classList.remove('is-hidden');            
+            const sesionButton = this.element.querySelector('.sesion-button');
+            sesionButton.classList.remove('is-hidden');
+            sesionButton.addEventListener('click', (event) => {
+            event.preventDefault();
+            window.localStorage.removeItem('token');
+            location.reload();
+            })
         } else {
             const loginRegisterButtons = this.element.querySelector('.login-register-buttons');
             loginRegisterButtons.classList.remove('is-hidden');
         }
     }
+
+    
 }
